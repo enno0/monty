18ce26aa6b896654dd341f85fb49b4b52bddd7d3
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-18 01:26:47 +0200

    c is fun :)

--- pstr.c
+++ pstr.c
@@ -8,37 +8,13 @@
 void pstr(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
-    char buffer[1024];
-    int index = 0;
-
     (void)line_number;
 
-    if (current->n >= 0 && current->n <= 127)
+    while (current != NULL && current->n > 0 && current->n < 128)
     {
-        while (current != NULL && current->n != 0)
-        {
-
-            if (index < (int)(sizeof(buffer) - 1))
-
-            {
-                buffer[index++] = current->n;
-            }
-            else
-            {
-                fprintf(stderr, "L%d: can't pstr, string too long\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-
-            current = current->next;
-        }
-
-        buffer[index] = '\0';
-
-        printf("%s\n", buffer);
+        printf("%c", (char)current->n);
+        current = current->next;
     }
-    else
-    {
-        fprintf(stderr, "L%d: can't pstr, value out of range\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+
+    printf("\n");
 }
